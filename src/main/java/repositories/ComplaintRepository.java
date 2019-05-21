3121e93d6a6a4f87ebf84495b69ebb3856acd6ba
miguelvr98 <miguelvr98@gmail.com>
2019-05-21 23:28:31 +0200

    Controladores terminados, falta revisar

--- src/main/java/repositories/ComplaintRepository.java
+++ src/main/java/repositories/ComplaintRepository.java
@@ -2,9 +2,20 @@ package repositories;
 
 import domain.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface ComplaintRepository extends JpaRepository<Complaint, Integer> {
 
+    @Query("select c from Complaint c where c.referee is null")
+    Collection<Complaint> getComplaintsWithNoReferee();
+
+    @Query("select c from Complaint c where c.referee.id=?1")
+    Collection<Complaint> getComplaintsByReferee(int refereeId);
+
+    @Query("select c from Complaint c where c.reader.id=?1")
+    Collection<Complaint> getComplaintsByReader(int readerId);
 }
