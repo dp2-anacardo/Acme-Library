696e5400394503769ffd7090716e87d77ddabca2
Servesus <jesusgarrocho1998@gmail.com>
2019-06-03 02:12:25 +0200

    Más test hechos

--- src/test/java/service/BookTest.java
+++ src/test/java/service/BookTest.java
@@ -1,121 +1,116 @@
+
 package service;
 
+import javax.transaction.Transactional;
 
-import domain.Book;
-import domain.Category;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.validation.DataBinder;
+
+import domain.Book;
 import services.BookService;
-import services.CategoryService;
 import utilities.AbstractTest;
 
-import javax.transaction.Transactional;
-import javax.validation.ValidationException;
-import java.util.ArrayList;
-import java.util.Collection;
-
 @ContextConfiguration(locations = {
-        "classpath:spring/junit.xml"
+	"classpath:spring/junit.xml"
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class BookTest extends AbstractTest {
 
-    @Autowired
-    private BookService bookService;
-
-    /*
-     * Testing functional requirement : 31.1 An actor who is authenticated as reader must be able to manage their books
-     * Positive: A reader create a book
-     * Negative: An organizer try create a book
-     * Sentence coverage:
-     * Data coverage:
-     */
-    @Test
-    public void createBookDriver(){
-        Object testingData[][] = {
-                {"title", "author","publisher", "español", "description", 100, "VERY GOOD",
-                        "9781234567897", "http://www.photo.com", "reader1", null},
-                {"title", "author","publisher", "español", "description", 100, "VERY GOOD",
-                        "9781234567897", "http://www.photo.com", "organizer1", IllegalArgumentException.class}
-        };
-        for(int i =0; i<testingData.length; i++)
-            this.createBookTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2],
-            (String) testingData[i][3], (String) testingData[i][4], (int) testingData[i][5], (String) testingData[i][6],
-                    (String) testingData[i][7], (String) testingData[i][8], (String) testingData[i][9],
-                    (Class<?>) testingData[i][10]);
-    }
-
-    private void createBookTemplate(String title, String author, String publisher,
-                                    String language, String description, int pageNumber,
-                                    String status, String isbn, String photo,
-                                    String user, Class<?> expected){
-
-        Class<?> caught;
-        caught = null;
-
-        try{
-            this.authenticate(user);
-            Book book = this.bookService.create();
-
-            book.setTitle(title);
-            book.setAuthor(author);
-            book.setPublisher(publisher);
-            book.setLanguageB(language);
-            book.setDescription(description);
-            book.setPageNumber(pageNumber);
-            book.setStatus(status);
-            book.setIsbn(isbn);
-            book.setPhoto(photo);
-
-            DataBinder binding = new DataBinder(new Book());
-            book = this.bookService.reconstruct(book, binding.getBindingResult());
-            this.bookService.save(book);
-        } catch (Throwable oops){
-            caught = oops.getClass();
-        }
-
-        super.checkExceptions(expected, caught);
-    }
+	@Autowired
+	private BookService bookService;
 
 
-    /*
-     * Testing functional requirement : 31.1 An actor who is authenticated as reader must be able to manage their books
-     * Positive: A reader edit a book
-     * Negative: An organizer try edit a book
-     * Sentence coverage:
-     * Data coverage:
-     */
-    @Test
-    public void editBookDriver(){
-        Object testingData[][] = {
-                {"book3", "prueba", "reader1", null},
-                {"book3", "prueba", "organizer1", IllegalArgumentException.class}
-        };
-        for(int i =0; i<testingData.length; i++)
-            this.editBookTemplate(super.getEntityId((String) testingData[i][0]), (String) testingData[i][1], (String) testingData[i][2],
-                    (Class<?>) testingData[i][3]);
-    }
+	/*
+	 * Testing functional requirement : 31.1 An actor who is authenticated as reader must be able to manage their books
+	 * Positive: A reader create a book
+	 * Negative: An organizer try create a book
+	 * Sentence coverage: 96%
+	 * Data coverage: 10%
+	 */
+	@Test
+	public void createBookDriver() {
+		final Object testingData[][] = {
+			{
+				"title", "author", "publisher", "español", "description", 100, "VERY GOOD", "9781234567897", "http://www.photo.com", "reader1", null
+			}, {
+				"title", "author", "publisher", "español", "description", 100, "VERY GOOD", "9781234567897", "http://www.photo.com", "organizer1", IllegalArgumentException.class
+			}
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.createBookTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (int) testingData[i][5], (String) testingData[i][6], (String) testingData[i][7],
+				(String) testingData[i][8], (String) testingData[i][9], (Class<?>) testingData[i][10]);
+	}
 
-    private void editBookTemplate(int entityId, String title, String user, Class<?> expected){
+	private void createBookTemplate(final String title, final String author, final String publisher, final String language, final String description, final int pageNumber, final String status, final String isbn, final String photo, final String user,
+		final Class<?> expected) {
 
-        Class<?> caught;
-        caught = null;
+		Class<?> caught;
+		caught = null;
 
-        try{
-            this.authenticate(user);
-            Book book = this.bookService.findOne(entityId);
-            book.setTitle(title);
-            DataBinder binding = new DataBinder(new Book());
-            book = this.bookService.reconstruct(book, binding.getBindingResult());
-            this.bookService.save(book);
-        } catch (Throwable oops){
-            caught = oops.getClass();
-        }
-        super.checkExceptions(expected, caught);
-    }
+		try {
+			this.authenticate(user);
+			Book book = this.bookService.create();
+
+			book.setTitle(title);
+			book.setAuthor(author);
+			book.setPublisher(publisher);
+			book.setLanguageB(language);
+			book.setDescription(description);
+			book.setPageNumber(pageNumber);
+			book.setStatus(status);
+			book.setIsbn(isbn);
+			book.setPhoto(photo);
+
+			final DataBinder binding = new DataBinder(new Book());
+			book = this.bookService.reconstruct(book, binding.getBindingResult());
+			this.bookService.save(book);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		super.checkExceptions(expected, caught);
+	}
+
+	/*
+	 * Testing functional requirement : 31.1 An actor who is authenticated as reader must be able to manage their books
+	 * Positive: A reader edit a book
+	 * Negative: An organizer try edit a book
+	 * Sentence coverage: 96%
+	 * Data coverage: 10%
+	 */
+	@Test
+	public void editBookDriver() {
+		final Object testingData[][] = {
+			{
+				"book3", "prueba", "reader1", null
+			}, {
+				"book3", "prueba", "organizer1", IllegalArgumentException.class
+			}
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.editBookTemplate(super.getEntityId((String) testingData[i][0]), (String) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
+	}
+
+	private void editBookTemplate(final int entityId, final String title, final String user, final Class<?> expected) {
+
+		Class<?> caught;
+		caught = null;
+
+		try {
+			this.authenticate(user);
+			Book book = this.bookService.findOne(entityId);
+			book.setTitle(title);
+			final DataBinder binding = new DataBinder(new Book());
+			book = this.bookService.reconstruct(book, binding.getBindingResult());
+			this.bookService.save(book);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		super.checkExceptions(expected, caught);
+	}
 }
--- src/test/java/service/EventTest.java
+++ src/test/java/service/EventTest.java
@@ -1,191 +1,194 @@
+
 package service;
 
-import domain.Event;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.transaction.Transactional;
+import javax.validation.ValidationException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.validation.DataBinder;
+
+import domain.Event;
 import services.EventService;
 import utilities.AbstractTest;
 
-import javax.transaction.Transactional;
-import javax.validation.Valid;
-import javax.validation.ValidationException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @ContextConfiguration(locations = {
-        "classpath:spring/junit.xml"
+	"classpath:spring/junit.xml"
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class EventTest extends AbstractTest {
-    @Autowired
-    EventService eventService;
 
-    /*
-     * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
-     * Positive: An organizer create an event in draft mode
-     * Negative: A reader try create an event in draft mode
-     * Sentence coverage:
-     * Data coverage:
-     */
-    @Test
-    public void createDraftEventDriver() {
-        final Object testingData[][] = {
-                {
-                        "prueba", "prueba", "02/02/2025 19:00","prueba",5, "organizer1", null
-                }, {
-                        "prueba", "prueba", "02/02/2025 19:00","prueba",5, "reader1", IllegalArgumentException.class
-        }
+	@Autowired
+	EventService eventService;
 
-        };
-        for (int i = 0; i < testingData.length; i++)
-            this.createDraftEventTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3],(int) testingData[i][4], (String) testingData[i][5],  (Class<?>) testingData[i][6]);
-    }
 
-    private void createDraftEventTemplate(final String title, final String description, final String date, String address, int maximumCapacity, String user, final Class<?> expected) {
-        Class<?> caught;
-        caught = null;
+	/*
+	 * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
+	 * Positive: An organizer create an event in draft mode
+	 * Negative: A reader try create an event in draft mode
+	 * Sentence coverage: 98%
+	 * Data coverage: 20%
+	 */
+	@Test
+	public void createDraftEventDriver() {
+		final Object testingData[][] = {
+			{
+				"prueba", "prueba", "02/02/2025 19:00", "prueba", 5, "organizer1", null
+			}, {
+				"prueba", "prueba", "02/02/2025 19:00", "prueba", 5, "reader1", IllegalArgumentException.class
+			}
 
-        try {
-            this.authenticate(user);
-            Event event = this.eventService.create();
-            event.setTitle(title);
-            event.setDescription(description);
-            final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-            final Date res = sdf.parse(date);
-            event.setDate(res);
-            event.setAddress(address);
-            event.setMaximumCapacity(maximumCapacity);
-            final DataBinder binding = new DataBinder(new Event());
-            event = this.eventService.reconstruct(event, binding.getBindingResult());
-            this.eventService.saveDraft(event);
-        } catch (final Throwable oops) {
-            caught = oops.getClass();
-        }
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.createDraftEventTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (int) testingData[i][4], (String) testingData[i][5], (Class<?>) testingData[i][6]);
+	}
 
-        super.checkExceptions(expected, caught);
-    }
+	private void createDraftEventTemplate(final String title, final String description, final String date, final String address, final int maximumCapacity, final String user, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
 
-    /*
-     * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
-     * Positive: An organizer create an event in final mode
-     * Negative: A reader try create an event in final mode
-     * Sentence coverage:
-     * Data coverage:
-     */
-    @Test
-    public void createFinalEventDriver() {
-        final Object testingData[][] = {
-                {
-                        "prueba", "prueba", "02/02/2025 19:00","prueba",5, "organizer1", null
-                }, {
-                "prueba", "prueba", "02/02/2025 19:00","prueba",5, "reader1", IllegalArgumentException.class
-        }
+		try {
+			this.authenticate(user);
+			Event event = this.eventService.create();
+			event.setTitle(title);
+			event.setDescription(description);
+			final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			final Date res = sdf.parse(date);
+			event.setDate(res);
+			event.setAddress(address);
+			event.setMaximumCapacity(maximumCapacity);
+			final DataBinder binding = new DataBinder(new Event());
+			event = this.eventService.reconstruct(event, binding.getBindingResult());
+			this.eventService.saveDraft(event);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
 
-        };
-        for (int i = 0; i < testingData.length; i++)
-            this.createFinalEventTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3],(int) testingData[i][4], (String) testingData[i][5],  (Class<?>) testingData[i][6]);
-    }
+		super.checkExceptions(expected, caught);
+	}
 
-    private void createFinalEventTemplate(final String title, final String description, final String date, String address, int maximumCapacity, String user, final Class<?> expected) {
-        Class<?> caught;
-        caught = null;
+	/*
+	 * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
+	 * Positive: An organizer create an event in final mode
+	 * Negative: A reader try create an event in final mode
+	 * Sentence coverage: 98%
+	 * Data coverage: 20%
+	 */
+	@Test
+	public void createFinalEventDriver() {
+		final Object testingData[][] = {
+			{
+				"prueba", "prueba", "02/02/2025 19:00", "prueba", 5, "organizer1", null
+			}, {
+				"prueba", "prueba", "02/02/2025 19:00", "prueba", 5, "reader1", IllegalArgumentException.class
+			}
 
-        try {
-            this.authenticate(user);
-            Event event = this.eventService.create();
-            event.setTitle(title);
-            event.setDescription(description);
-            final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-            final Date res = sdf.parse(date);
-            event.setDate(res);
-            event.setAddress(address);
-            event.setMaximumCapacity(maximumCapacity);
-            final DataBinder binding = new DataBinder(new Event());
-            event = this.eventService.reconstruct(event, binding.getBindingResult());
-            this.eventService.saveFinal(event);
-        } catch (final Throwable oops) {
-            caught = oops.getClass();
-        }
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.createFinalEventTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (int) testingData[i][4], (String) testingData[i][5], (Class<?>) testingData[i][6]);
+	}
 
-        super.checkExceptions(expected, caught);
-    }
+	private void createFinalEventTemplate(final String title, final String description, final String date, final String address, final int maximumCapacity, final String user, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
 
-    /*
-     * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
-     * Positive: An organizer edit an event
-     * Negative: A reader try edit an event. An organizer try edit an event with invalid data.
-     * Sentence coverage:
-     * Data coverage:
-     */
-    @Test
-    public void editEventDriver() {
-        final Object testingData[][] = {
-                {
-                        "event2", "pruebaEdit", "organizer1", null
-                }, {
-                        "event2", "pruebaEdit", "reader1", IllegalArgumentException.class
-                }, {
-                        "event2", "", "organizer1", ValidationException.class
-                }
+		try {
+			this.authenticate(user);
+			Event event = this.eventService.create();
+			event.setTitle(title);
+			event.setDescription(description);
+			final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			final Date res = sdf.parse(date);
+			event.setDate(res);
+			event.setAddress(address);
+			event.setMaximumCapacity(maximumCapacity);
+			final DataBinder binding = new DataBinder(new Event());
+			event = this.eventService.reconstruct(event, binding.getBindingResult());
+			this.eventService.saveFinal(event);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
 
-        };
-        for (int i = 0; i < testingData.length; i++)
-            this.editEventTemplate(super.getEntityId((String) testingData[i][0]), (String) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
-    }
-    private void editEventTemplate(int entityId, String title, String user, Class<?> expected ){
-        Class<?> caught;
-        caught = null;
+		super.checkExceptions(expected, caught);
+	}
 
-        try{
-            this.authenticate(user);
-            Event event = this.eventService.findOne(entityId);
-            event.setTitle(title);
-            DataBinder binding = new DataBinder(new Event());
-            event = this.eventService.reconstruct(event, binding.getBindingResult());
-            this.eventService.saveFinal(event);
-        } catch (Throwable oops){
-            caught = oops.getClass();
-        }
-        super.checkExceptions(expected,caught);
-    }
+	/*
+	 * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
+	 * Positive: An organizer edit an event
+	 * Negative: A reader try edit an event. An organizer try edit an event with invalid data.
+	 * Sentence coverage: 98%
+	 * Data coverage: 66%
+	 */
+	@Test
+	public void editEventDriver() {
+		final Object testingData[][] = {
+			{
+				"event2", "pruebaEdit", "organizer1", null
+			}, {
+				"event2", "pruebaEdit", "reader1", IllegalArgumentException.class
+			}, {
+				"event2", "", "organizer1", ValidationException.class
+			}
 
-    /*
-     * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
-     * Positive: An organizer delete an event
-     * Negative: A reader try delete an event
-     * Sentence coverage:
-     * Data coverage:
-     */
-    @Test
-    public void deleteEventDriver() {
-        final Object testingData[][] = {
-                {
-                        "event2", "organizer1", null
-                }, {
-                "event2", "reader1", IllegalArgumentException.class
-        }
-        };
-        for (int i = 0; i < testingData.length; i++)
-            this.deleteEventTemplate(super.getEntityId((String) testingData[i][0]), (String) testingData[i][1], (Class<?>) testingData[i][2]);
-    }
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.editEventTemplate(super.getEntityId((String) testingData[i][0]), (String) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
+	}
+	private void editEventTemplate(final int entityId, final String title, final String user, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
 
-    private void deleteEventTemplate(int entityId, String user, Class<?> expected){
-        Class<?> caught;
-        caught = null;
+		try {
+			this.authenticate(user);
+			Event event = this.eventService.findOne(entityId);
+			event.setTitle(title);
+			final DataBinder binding = new DataBinder(new Event());
+			event = this.eventService.reconstruct(event, binding.getBindingResult());
+			this.eventService.saveFinal(event);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		super.checkExceptions(expected, caught);
+	}
 
-        try {
-            this.authenticate(user);
-            Event event = this.eventService.findOne(entityId);
-            this.eventService.delete(event);
-        } catch (final Throwable oops) {
-            caught = oops.getClass();
-        }
-        super.checkExceptions(expected, caught);
-    }
+	/*
+	 * Testing functional requirement : 32.1 An actor who is authenticated as organizer must be able to manage their events
+	 * Positive: An organizer delete an event
+	 * Negative: A reader try delete an event
+	 * Sentence coverage: 92%
+	 * Data coverage: Not applicable
+	 */
+	@Test
+	public void deleteEventDriver() {
+		final Object testingData[][] = {
+			{
+				"event2", "organizer1", null
+			}, {
+				"event2", "reader1", IllegalArgumentException.class
+			}
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.deleteEventTemplate(super.getEntityId((String) testingData[i][0]), (String) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+
+	private void deleteEventTemplate(final int entityId, final String user, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+
+		try {
+			this.authenticate(user);
+			final Event event = this.eventService.findOne(entityId);
+			this.eventService.delete(event);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		super.checkExceptions(expected, caught);
+	}
 }
-
